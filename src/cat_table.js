23c80ee9b236f82b4c91bfc6ca6046601c918ba8
Pouya Soltani <pouyasoltani81@gmail.com>
2024-11-30 12:25:47 -0800

    p

--- src/cat_table.js
+++ src/cat_table.js
@@ -135,7 +135,7 @@ function renderSubcategories(subcategories, level) {
         const buttons = `
             <div class="flex space-x-2 mt-2 justify-evenly">
                 <button class="px-3 py-1 bg-teal-500 text-white rounded-md text-sm" onclick="gotocharts('${subcategory.name_fa}','${subcategory.slug_fa}')">اطلاعات بیشتر</button>
-                ${isLastLevel ?  `<button class="px-3 py-1 bg-blue-500 text-white rounded-md text-sm p-4" onclick="gotoproducts('${subcategory.name_fa}')">لیست پروداکت ها</button>` : ``}
+                ${isLastLevel ?  `<button class="px-3 py-1 bg-blue-500 text-white rounded-md text-sm p-4" onclick="gotoproducts('${subcategory.name_fa}','${subcategory.slug_fa}')">لیست پروداکت ها</button>` : ``}
             </div>
         `;
         return `
@@ -160,7 +160,7 @@ async function initialize_PAGE() {
     }
 }
 
-function gotoproducts(name) {
+function gotoproducts(name,slug) {
     console.log(name);
     
     const data = {
