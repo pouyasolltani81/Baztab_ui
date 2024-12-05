9f826b840afaa4c5cb700283c4115cd500940ac1
pouyasolltani81 <113523357+pouyasolltani81@users.noreply.github.com>
2024-12-05 16:42:12 +0330

    info section

--- src/table_p.js
+++ src/table_p.js
@@ -530,8 +530,10 @@ slug.innerHTML = `<div class='flex flex-col'>
             // Save the selected rows' data to localStorage
             localStorage.setItem('selectedProducts', JSON.stringify(selectedProducts));
 
+            console.log(selectedProducts);
+            
             // Redirect to p_info.html
-            window.location.href = 'p_info.html';
+            // window.location.href = 'p_info.html';
           });
         }
       } else {
