678ceb4a40239b69f7216456db5571ea8ba513ff
pouyaWork <pouyasoltani81@gmail.com>
2024-12-14 09:10:04 +0330

    more info chat bot

--- src/chatbot.js
+++ src/chatbot.js
@@ -50,7 +50,7 @@ async function sendMessage(userm , token) {
 
     const data = await response.json();
     console.log(data);
-    fetchAllProducts(data.data.product_list)
+    await fetchAllProducts(data.data.product_list)
     console.log('booo',all_products);
     
 
@@ -224,7 +224,7 @@ startChatBtn.addEventListener('click', async () => {
             } else {
                 setTimeout(() => {
                     addProductCards(all_products);
-                }, 1500); 
+                }, 10); 
             }
         }
 
@@ -320,15 +320,18 @@ function addProductCards(products) {
         const productCard = `<div class="bg-white p-4 shadow-lg rounded-lg border border-gray-300">
                                 <h3 class="text-teal-600 font-semibold text-center">${product.product_name_fa}</h3>
                                 <p class="text-gray-600 text-center">${(product.price_stat.avg/10).toLocaleString()} تومان</p>
-                                <button class="bg-teal-500 text-white px-4 py-2 rounded-lg mt-2 w-full hover:bg-teal-400 transition duration-200">
-                                    <i class="fas fa-cart-plus mr-2"></i> افزودن به سبد خرید
+                                <button class="bg-teal-500 text-white px-4 py-2 rounded-lg mt-2 w-full hover:bg-teal-400 transition duration-200" onclick='gotoproductinfo('${product}')'>
+                                    <i class="fas fa-cart-plus mr-2"></i> More info
                                 </button>
                               </div>`;
         productGrid.innerHTML += productCard;
     });
 }
 
-
+function gotoproductinfo(product) {
+    console.log(product);
+    
+}
 
 function backAndReload() {
 
