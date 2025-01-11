e09cfcb957877ee5efed10419e1e43a87b5b2544
pouyaWork <pouyasoltani81@gmail.com>
2025-01-11 08:28:28 +0330

    ui

--- src/chatbot.js
+++ src/chatbot.js
@@ -748,13 +748,13 @@ function addProductCards(products) {
     
     productGrid.innerHTML = '';
     products.forEach(product => {
-        const productCard = `<div class="bg-white p-4 shadow-lg rounded-lg border border-gray-300">
+        const productCard = `<div class="bg-white p-4 shadow-lg rounded-lg border border-gray-300 h-fit">
                                 <h3 class="text-teal-600 font-semibold text-center">${product.product_name_fa}</h3>
                                 ${product.relational_data.price_stat ? `<p class="text-gray-600 text-center">${(product.price_stat.avg/10).toLocaleString()} تومان</p>` : ''}
 
                                 <img src="${product.relational_data.media_info.primary_image}" class='rounded-lg h-[100px] w-full' alt="Product image">
                                 <button class="bg-teal-500 text-white px-4 py-2 rounded-lg mt-2 w-full hover:bg-teal-400 transition duration-200" onclick="gotoproductinfo('${product.relational_data._id}')">
-                                    <i class="fas fa-cart-plus mr-2"></i> More info
+                                    <i class="fas fa-cart-plus mr-2"></i> اطلاعات بیشتر
                                 </button>
                               </div>`;
         productGrid.innerHTML += productCard;
