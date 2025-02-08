c293d9ddf53b97e27b24d0828a7ff7993a9f0bc8
pouyaWork <pouyasoltani81@gmail.com>
2025-02-08 12:27:50 +0330

    update

--- src/brand_js.js
+++ src/brand_js.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
  
-     console.log(JSON.parse(sessionStorage.getItem('brand_cat')));
+     document.getElementById('brand_info_title').innerHTML = JSON.parse(sessionStorage.getItem('brand_cat')).name;
      
   fetch('http://79.175.177.113:21800/Brands/get_brands_by_category_id/', {
             method: 'POST',
