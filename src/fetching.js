feef198a221a01cbaccb0e637738c5833621b287
pouyaWork <pouyasoltani81@gmail.com>
2025-02-24 10:01:27 +0330

    update

---
+++ src/fetching.js
@@ -0,0 +1,31 @@
+async function connect_to_server(endpoint, method, header, type, body, route) {
+
+    let header_c = {
+        'Content-Type': type,
+        "Accept-Version": 1,
+        'Accept': "application/json",
+        "Access-Control-Allow-Origin": "*",
+
+    }
+
+    header_c.append(header)
+
+    try {
+
+
+        return await fetch(endpoint, {
+            method: method,
+            headers: header_c,
+            body: body
+        });
+
+
+    }
+
+    catch (e) {
+
+
+        console.log('error from :', route, 'it says :', e)
+    }
+
+} 
