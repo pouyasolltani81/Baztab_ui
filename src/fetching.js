2d2f15732475ba837f47362861c8526fbb1c717c
pouyaWork <pouyasoltani81@gmail.com>
2025-02-26 09:22:51 +0330

    update

--- src/fetching.js
+++ src/fetching.js
@@ -2,7 +2,7 @@ const log = console.log
 async function connect_to_server(endpoint, method, user_token, type, body, route) {
 
     let header_c = {
-        'Content-Type': type,
+        // 'Content-Type': type,
         "Accept-Version": 1,
         'Accept': "application/json",
         "Access-Control-Allow-Origin": "*",
@@ -27,8 +27,6 @@ async function connect_to_server(endpoint, method, user_token, type, body, route
     }
 
     catch (e) {
-
-
         console.log('error from :', route, 'it says :', e)
     }
 
